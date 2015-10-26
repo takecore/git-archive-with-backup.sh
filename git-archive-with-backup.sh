458165e4ae46c56ea1d393bbf60226149ec49922
Takehiro Ohtani <ohtani@impv.co.jp>
2015-10-26 17:43:44 +0900

    Revert "[fix] 削除ファイル混在時にリスト化をフィルタ"
    This reverts commit 46130e7bba6fbbda52507c197e39cd4c34fe8b79.

--- git-archive-with-backup.sh
+++ git-archive-with-backup.sh
@@ -20,7 +20,7 @@ BACKUP_ARCHIVE_PATH=$HOME/Desktop/backup_$FROM_RIVISION.zip
 
 # archive start
 git diff --name-only $TO_RIVISION $FROM_RIVISION > $DIFF_LIST_PATH
-git archive --format=zip $TO_RIVISION `git diff --diff-filter=D --name-only $TO_RIVISION $FROM_RIVISION` -o $RELEASE_ARCHIVE_PATH
+git archive --format=zip $TO_RIVISION `git diff --name-only $TO_RIVISION $FROM_RIVISION` -o $RELEASE_ARCHIVE_PATH
 git checkout $FROM_RIVISION
 zip $BACKUP_ARCHIVE_PATH -@ < $DIFF_LIST_PATH
 rm $DIFF_LIST_PATH
